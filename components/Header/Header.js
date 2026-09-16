9615cdee18a0551a8d16d44c42cd51da044e08f6
niiima <nima.bastani@gmail.com>
2026-09-16 22:02:04 +0330

    Fix and update Header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import styled from "styled-components";
+
 // import SvgSettingButton from "../svgs/SvgSettingButton";
 import PageNavigation from "./PageNavigation";
 // import LogoDropSvg from "../svgs/LogoDropSvg";
@@ -8,15 +9,14 @@ import UIContext from "../../context/UIContext.js";
 
 export const HeaderBar = styled.header`
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: row;
   justify-content: space-between;
-  /* padding: 0.5rem; */
-  background: linear-gradient(135deg, #11e7df 0%, #39f 50%, #b490ca 100%);
-  /* flex: 0 0 1; */
-  /* flex: 0 0 1; */
-  /* flex: 0 1 0; */
-  height: 8svh;
+  align-items: middle;
+  background: #111111;
+  border-bottom: 1px solid #222;
+  height: 8 svh;
   width: 100%;
+  padding: 0 0.1;
 `;
 
 const Header = ({ children }) => {
@@ -24,19 +24,19 @@ const Header = ({ children }) => {
 
   return (
     <HeaderBar>
-      {children}
-      {/* <img src={GPTLogo} style={{ backgroundColor: "transparent" }}></img> */}
-      {/* <LogoDropSvg></LogoDropSvg> */}
-      {/* <SvgSettingButton color='gold' backgroundColor='transparent' /> */}
-      <PageNavigation></PageNavigation>
-      {/* <HamburgerIcon handleClick={handleClick} open={open} /> */}
-      {/* <ToggleButton onClick={handleClick}>Toggle Sidebar</ToggleButton> */}
+      {/* HamburgerIcon is now first, so it appears on the left */}
       <HamburgerIcon
         color='white'
         background='white'
         open={asideExpanded}
         handleClick={setAsideExpand}
       />
+      
+      {/* The content div is now second, so it appears on the right */}
+      <div style={{ display: 'flex', alignItems: 'center', gap: '2rem' }}>
+        {children}
+        <PageNavigation />
+      </div>
     </HeaderBar>
   );
 };
--- components/Header/PageNavigation.js
+++ components/Header/PageNavigation.js
@@ -2,55 +2,35 @@ import { MdAirlineStops, MdMultipleStop } from "react-icons/md";
 import { useContext } from "react";
 import { Flex } from "../Atoms/Flex";
 import { FlexItem } from "../Atoms/FlexItem";
-import { GiAbstract037 } from "react-icons/gi";
+// import { GiAbstract037 } from "react-icons/gi";
 import styled from "styled-components";
 import Link from "next/link";
 import AIContext from "../../context/AIContext";
 // import { FcPicture } from "react-icons/fc";
-import { BsSpotify } from "react-icons/bs";
+// import { BsSpotify } from "react-icons/bs";
 // import { VscGithubAction } from "react-icons/vsc";
 
 const ICON_SIZE = 26;
 const NavigationWrapper = styled(Flex)`
-  min-width: 80vw;
-
-  justify-content: space-evenly;
+  display: flex;
   align-items: center;
-  & > :first-of-type {
-    padding-left: 5vw;
-  }
+  gap: 1rem;
+  padding: 0 1rem;
 `;
 
 const LinkItem = styled(FlexItem)`
-  /* max-width: 18%; */
-  @media only screen and (max-width: 780px) {
-    font-size: 12px;
-    & .text-container {
-      display: none;
-    }
-    & .icon-container {
-      padding-top: 0 !important ;
+  font-size: 14px;
+  color: #ccc;
+  text-decoration: none;
+  transition: color 0.2s;
 
-      margin-top: -2svh !important ;
-    }
-  }
-
-  @media only screen and (max-width: 480px) {
-    font-size: 10px;
-    & > :first-of-type {
-      padding-left: 9vw;
-    }
-  }
-  @media only screen and (min-width: 1260px) {
-    font-size: 15px;
-  }
-  & * {
+  &:hover {
     color: white;
   }
+
   &.active {
-    & * {
-      color: #123699;
-    }
+    color: white;
+    font-weight: 600;
   }
 `;
 
@@ -61,37 +41,8 @@ const routes = [
     icon: <MdMultipleStop size={ICON_SIZE} />,
     text: "Conversation",
   },
-  // {
-  //   id: 1,
-  //   url: "/single",
-  //   icon: <MdAirlineStops size={ICON_SIZE} />,
-  //   text: "Messaging",
-  // },
-  // // {
-  // //   id: 2,
-  // //   url: "/acts",
-  // //   icon: <VscGithubAction size={ICON_SIZE} />,
-  // //   text: "Instant Act",
-  // // },
-  // {
-  //   id: 3,
-  //   url: "/stream",
-  //   icon: <GiAbstract037 size={ICON_SIZE} />,
-  //   text: "GPT3",
-  // },
-  // {
-  //   id: 4,
-  //   url: "/art",
-  //   icon: <FcPicture color='white' size={ICON_SIZE} />,
-  //   text: "Painting",
-  // },BsSpotify
-  // {
-  //   id: 4,
-  //   url: "/playlist",
-  //   icon: <BsSpotify color="green" size={ICON_SIZE} />,
-  //   text: "Playlist",
-  // },
 ];
+
 const PageNavigation = () => {
   const { activeRoute, setActiveRoute } = useContext(AIContext);
 
@@ -99,34 +50,24 @@ const PageNavigation = () => {
     <NavigationWrapper>
       {routes.map((route) => (
         <LinkItem
-          lineHeight={2}
-          fontWeight={"normal"}
-          fontSize={2}
-          className={`${activeRoute === route.id ? "active" : ""}`}
           key={route.url}
+          className={`${activeRoute === route.id ? "active" : ""}`}
         >
-          <Link href={route.url} onClick={() => setActiveRoute(route.id)}>
-            <i
-              className="icon-container"
-              style={{
-                position: "absolute",
-                marginLeft: -27,
-              }}
-            >
-              {route.icon}
-            </i>
-            <span
-              className="text-container"
-              style={{ display: "absolute", cursor: "pointer" }}
-              onClick={() => setActiveRoute(route.id)}
-            >
-              {route.text}
-            </span>
+          <Link
+            href={route.url}
+            onClick={(e) => {
+              e.preventDefault();
+              setActiveRoute(route.id);
+            }}
+            style={{ display: 'flex', alignItems: 'center', gap: '8px', textDecoration: 'none', color: 'inherit' }}
+          >
+            {route.icon}
+            <span className="text-container">{route.text}</span>
           </Link>
         </LinkItem>
       ))}
     </NavigationWrapper>
   );
 };
-
 export default PageNavigation;
+
